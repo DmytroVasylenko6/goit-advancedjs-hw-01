959a8c68d4b67ca9e345c2d2aa0ef901e5794c52
DmytroVasylenko6 <tredstoun651@ukr.net>
2024-10-19 21:41:07 +0300

    add files

---
+++ src/js/2-form.js
@@ -0,0 +1,35 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'form-state';
+const form = document.querySelector('.form');
+const { email, message } = form.elements;
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+reloadPage();
+
+function onInputData() {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
+}
+
+function reloadPage() {
+  email.value = dataForm.email || '';
+  message.value = dataForm.message || '';
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all required fields.');
+  }
+
+  console.log({ email: email.value, message: message.value });
+
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+  dataForm = {};
+}
